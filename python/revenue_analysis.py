a57b794a73aab07e78e0b0baa9d6616141651e1f
Arun Kothandaraman <arunsep95@gmail.com>
2026-07-17 20:56:39 -0700

    Delete python/revenue_analysis.py

--- python/revenue_analysis.py
+++
@@ -1,9 +0,0 @@
-# Revenue Intelligence Dashboard
-# Python Analysis
-
-# This script will perform:
-# 1. Data cleaning
-# 2. Exploratory Data Analysis (EDA)
-# 3. Revenue analysis
-# 4. Customer analysis
-# 5. Export cleaned dataset for Power BI
